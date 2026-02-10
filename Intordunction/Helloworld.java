d6725633033c9d1e1e1bcf43c7ed479fe50dd46c
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-10 16:07:58 +0530

    Add Hello World

---
+++ Intordunction/Helloworld.java
@@ -0,0 +1,10 @@
+/*package whatever //do not write package name here */
+
+import java.lang.*;
+
+class Helloworld {
+	public static void main (String[] args) {
+		//code here
+		System.out.println("Hello World");
+	}
+}
