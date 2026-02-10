afae1778fdb92591a19c2fc50a2bb8b5c4f854f0
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-10 20:47:29 +0530

    add input float number

---
+++ Input-Output/Inputfloat.java
@@ -0,0 +1,11 @@
+//Back-end complete function Template for Java
+import java.util.*;
+
+class Inputfloat {
+    public static void main(String args[]) {
+        // Take a Float input and Print
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        System.out.println(a*10);
+    }
+}
