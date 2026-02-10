7a0ee8a28b24c78ef2f6944d29f169a1034429c4
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-10 20:46:30 +0530

    add taking string file

---
+++ Input-Output/Inputstring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Inputstring {
+    public static void main(String args[]) {
+        // Take String Input and Print it
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+    }
+}
