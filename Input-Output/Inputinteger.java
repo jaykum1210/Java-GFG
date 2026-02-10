92fee188ba154f06d298d84d7d11fee527b688f1
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-10 20:45:35 +0530

    add taking integer file

---
+++ Input-Output/Inputinteger.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Inputinteger {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        //  Take input
+        //  Add 10 and Print
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a+10);
+    }
+}
