61d5559f17c65d10fb9661860d3e4402d192abd5
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-10 16:12:50 +0530

    add second comment file

---
+++ Intordunction/Comment2.java
@@ -0,0 +1,9 @@
+
+class Comment2 {
+    public static void main(String args[]) {
+        // Prints Hello World
+        // System.out.println("Hello World");
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+    }
+}
