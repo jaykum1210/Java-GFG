7e9dad6cec5093ace2bb150bd71dd6638d43264b
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-10 16:40:47 +0530

    add type caste file

---
+++ Variable-DataTypes/TypeCaste.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class TypeCaste {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        // Typecast and print
+        System.out.println((int)d);
+    }
+}
